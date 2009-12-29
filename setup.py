bf3f2d798dcf62e7fdd49d6d35c8ce42b1be57c3
Guillaume Roguez <yomgui1@gmail.com>
2009-12-29 14:50:31 +0000

    dump to v0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup, Extension
 opt = ['-Wall -Wuninitialized -Wstrict-prototypes']
 
 setup(name = 'PyMUI',
-      version = '0.1',
+      version = '0.2',
       author='Guillaume Roguez',
       description = 'Python wrapper for the MUI library',
       url='http://www.yomgui.fr/yiki/doku.php/dev:pymui:start',
