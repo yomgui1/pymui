f8ff5bc1fc6446dae4e82ee3dc8be7f71fd0d598
Guillaume Roguez <yomgui1@gmail.com>
2011-09-17 09:35:24 +0000

    [Core] attr added to Dtpic
    --HG--
    branch : redesign_2010

--- pymui/_core.py
+++ pymui/_core.py
@@ -1465,6 +1465,7 @@ class Dtpic(Area):
     MinWidth  = MAttribute(MUIA_Dtpic_MinWidth,  'i.g', c_BOOL)
     MinHeight = MAttribute(MUIA_Dtpic_MinHeight, 'i.g', c_BOOL)
     Scale     = MAttribute(MUIA_Dtpic_Scale,     'isg', c_LONG)
+    LightenOnMouse = MAttribute(MUIA_Dtpic_LightenOnMouse, 'i.g', c_BOOL)
 
     def __init__(self, Name=None, **kwds):
         if Name: kwds['Name'] = Name
